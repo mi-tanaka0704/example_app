5fc02b42f58557dd42885114d01831efe9e1f9bd
mi-tanaka0704 <mi2501130842@gmail.com>
2026-01-11 23:53:49 +0900

    Listまとめ完了

--- lib/main_list.dart
+++ lib/main_list.dart
@@ -32,9 +32,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  // listを使用してみる
+  final List<int> _history = [];
+
   void _incrementCounter() {
     setState(() {
       _counter++;
+
+      // カウントした数字をListに追加
+      _history.add(_counter);
     });
   }
 
